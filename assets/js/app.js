bef3f2d4fed1440049e534704ce923c321ccf6c1
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-09 15:22:47 -0700

    set up firebase config comms, and create model objects

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+var players = []
+
+var p1 = {
+    name: "",
+    choice: "",
+    chat: [],
+}
+
+var p2 = {
+    name: "",
+    choice: "",
+    chat: [],
+}
+
+var config = {
+    apiKey: "AIzaSyAWjLYbsoBa1CqDluBd2GGZrHEb17ijO64",
+    authDomain: "test-a048d.firebaseapp.com",
+    databaseURL: "https://test-a048d.firebaseio.com",
+    projectId: "test-a048d",
+    storageBucket: "test-a048d.appspot.com",
+    messagingSenderId: "859937864573",
+    appId: "1:859937864573:web:29b9c684579f86bd"
+};
+
+firebase.initializeApp(config);
+
+firebase.initializeApp(config);
