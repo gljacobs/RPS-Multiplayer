4a5e5bb79bbeb4d9b0b9bf30f57b94cd1cbc5b11
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-09 18:53:00 -0700

    update firebase config, database calls

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,50 @@
-var players = []
+var players = [];
 
 var p1 = {
     name: "",
     choice: "",
     chat: [],
-}
+    exists: false
+};
 
 var p2 = {
     name: "",
     choice: "",
     chat: [],
-}
+    exists: false,
+};
 
 var config = {
-    apiKey: "AIzaSyAWjLYbsoBa1CqDluBd2GGZrHEb17ijO64",
-    authDomain: "test-a048d.firebaseapp.com",
-    databaseURL: "https://test-a048d.firebaseio.com",
-    projectId: "test-a048d",
-    storageBucket: "test-a048d.appspot.com",
-    messagingSenderId: "859937864573",
-    appId: "1:859937864573:web:29b9c684579f86bd"
+    apiKey: "AIzaSyDNG5Ze8prVwVwa7GKyX_t_7Vc6H743xy8",
+    authDomain: "rps-multi-12251.firebaseapp.com",
+    databaseURL: "https://rps-multi-12251.firebaseio.com",
+    projectId: "rps-multi-12251",
+    storageBucket: "rps-multi-12251.appspot.com",
+    messagingSenderId: "598644354715",
+    appId: "1:598644354715:web:064bb00c1ae7eafa"
 };
 
 firebase.initializeApp(config);
 
-firebase.initializeApp(config);
+var database = firebase.database();
+
+$("#add-player").on("click", (e) => {
+    e.preventDefault();
+
+    if (!p1.exists) {
+        p1.name = $("#player-in").val();
+        $("#player-in").val("");
+        p1.exists = true;
+        database.ref().set({
+            player1: p1
+        });
+    }
+    if (!p2.exists) {
+        p2.name = $("#player-in").val();
+        $("#player-in").val("");
+        p2.exists = true;
+        database.ref().set({
+            player2: p2
+        });
+    }
+});
